e6954d83e84295b89b3820245a661262a83d7c21
omram89 <ogatla@iastate.edu>
2018-09-17 17:43:41 -0500

    merged raid to df

--- spatial/spat.py
+++ spatial/spat.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 #from dataloader import load_joined_ems_events
 
 COLNAMES_EMS_EVENTS = 'timestamp,seqID,eventType,severity,nodeName,stack,shelf,bay,dev,componentName'.split(',')
-
+COLNAMES_RAID_GROUP = 'raidGroup,stack,shelf,bay,dev,type'.split(',')
 def load_ems_events(path=None, low_memory=True, delim=None):
 
     ems_glob = os.path.join(path, 'ems-events-split*.csv')
@@ -20,17 +20,30 @@ def load_ems_events(path=None, low_memory=True, delim=None):
     
     for filename in sorted(glob.iglob(ems_glob)):
         df = pd.read_csv(filename, delimiter=delim, header=None, names=COLNAMES_EMS_EVENTS, low_memory=low_memory)
+
+        df['stack'].replace('NULL', np.nan, inplace=True)
+        df['stack'].replace('', np.nan, inplace=True)
+        df['shelf'].replace('', np.nan, inplace=True)
+        df.dropna(subset=['stack'], inplace=True)
+        #df = df[df.shelf != '?']
+
+
+        raid_filename = filename.replace('ems-events','raid-groups')
+        raid_df = pd.read_csv(raid_filename, delimiter=',', header=None, names=COLNAMES_RAID_GROUP, low_memory=low_memory)
+        raid_df = raid_df.drop_duplicates(subset=COLNAMES_RAID_GROUP)
+
+        merged = df.merge(raid_df, how='left', left_on=['stack','shelf','bay','dev'], right_on=['stack','shelf','bay','dev'], sort=False, suffixes=('',''))
         if ret_df is None:
-            ret_df = df
+            ret_df = merged
         else:
-            ret_df = pd.concat([df, ret_df])
+            ret_df = pd.concat([merged, ret_df])
 
     #ret_df = ret_df[np.isfinite(ret_df['stack'])]
-    ret_df['stack'].replace('NULL', np.nan, inplace=True)
-    ret_df['stack'].replace('', np.nan, inplace=True)
-    ret_df['shelf'].replace('', np.nan, inplace=True)
+    #ret_df['stack'].replace('NULL', np.nan, inplace=True)
+    #ret_df['stack'].replace('', np.nan, inplace=True)
+    #ret_df['shelf'].replace('', np.nan, inplace=True)
 
-    ret_df.dropna(subset=['stack'], inplace=True)
+    #ret_df.dropna(subset=['stack'], inplace=True)
     ret_df.sort_values(['timestamp','seqID'], inplace=True)
     return ret_df
     #ret_df = ret_df.join(ret_df)
@@ -38,21 +51,27 @@ def load_ems_events(path=None, low_memory=True, delim=None):
 
 final_df = load_ems_events('/home/om/Downloads/NetApp_dataset/20170522-SystemA/systemA', True,',')
 
-final_df['shelf'] = pd.Series([], dtype=object)
-
-#final_df.to_csv('/home/om/Downloads/data_figures/final_df.csv',sep=',')
+#final_df['shelf'] = pd.Series([], dtype=object)
+#final_df = final_df[final_df.shelf != '?']
+final_df['shelf'] = final_df['shelf'].astype(np.uint64)
+#final_df.to_csv('/home/om/Downloads/data_figures/final_df_raid.csv',sep=',')
+#final_df.sort_values(['eventType','nodeName','stack','shelf','bay'],inplace=True)
+#gb1.last().sort_values('nodeName').to_csv('/home/om/Downloads/data_figures/gb_evnt_node.csv',sep=',')
+#final_df.to_csv('/home/om/Downloads/data_figures/final_df_all.csv',sep=',')
 #print(len(final_df.index))
-err_cnt = len(final_df.index)
-n_err_cnt = 8 * err_cnt // 10
-print(n_err_cnt)
+#err_cnt = len(final_df.index)
+#n_err_cnt = 8 * err_cnt // 10
+#print(n_err_cnt)
 
 #cnt_plt = final_df.groupby("bay").ids.agg(lambda x:len(x.unique()))
 
 #pd.value_counts(final_df['bay']).plot(kind="bar")
 #sr = pd.value_counts(final_df['bay'])
-sr = pd.value_counts(final_df['shelf'])
-sr.plot(kind="bar")
-print(sr)
+#sr = pd.value_counts(final_df['shelf'])
+sr = pd.value_counts(final_df['raidGroup'])
+#sr.plot(kind="bar")
+#print(sr)
+sr.to_csv('/home/om/Downloads/data_figures/raid_plot.csv',',')
 
 #print(final_df.head(1000))
 #final_df.hist(column='bay', bins=24)
@@ -61,5 +80,5 @@ print(sr)
 #bay_fig = bay_hist.get_figure()
 #bay_fig.savefig('./bay_hist.pdf')
 #bay_hist.plot()
-plt.show()
+#plt.show()
 
